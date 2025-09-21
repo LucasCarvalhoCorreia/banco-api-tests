02f15c922656d959a15a530fc3e1d477543456ab
Lucas Correia <lucas.carvalho79@hotmail.com>
2025-09-20 23:00:03 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- test/login.test.js
+++ test/login.test.js
@@ -1,16 +1,17 @@
 const request = require('supertest');
 const { expect } = require('chai');
+const postLogin = require('../fixtures/postLogin.json');
+require('dotenv').config();
 
 describe('Login', () => {
     describe('POST /login', () => {
         it('Deve retornar 200 com token em string quando usar credenciais válidas', async () => {
+            const bodyLogin = { ...postLogin };
+
             const resposta = await request('http://localhost:3000')
                 .post('/login')
                 .set('Content-Type', 'application/json')
-                .send({
-                    "username": 'julio.lima',
-                    "senha": '123456'
-                });
+                .send(bodyLogin);
 
             expect(resposta.status).to.be.equal(200);
             expect(resposta.body.token).to.be.a('string');
