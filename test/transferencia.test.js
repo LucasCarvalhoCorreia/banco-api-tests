3aa202f49553054749d6ee434d9adfa5a58edf09
Lucas Correia <lucas.carvalho79@hotmail.com>
2025-09-20 22:43:02 -0300

    Adicionando o hook de beforeach para captura do token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,10 +5,14 @@ require('dotenv').config();
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
+        let token;
+        beforeEach(async () => {
+            // Configurar o ambiente antes de cada teste, se necessário
+            token = await obterToken('julio.lima', '123456');
+        });
+
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de 10 reais', async () => {
             // Capturar o token
-            const token = await obterToken('julio.lima', '123456');
-
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
@@ -26,8 +30,6 @@ describe('Transferencias', () => {
 
         it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de 10 reais', async () => {
             // Capturar o token
-            const token = await obterToken('julio.lima', '123456');
-
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
